df1ad6227d51319a64168fc749ee5ca408d4ac26
diarra9 <amadiarra01@gmail.com>
2024-01-31 17:14:19 +0100

    Add function intersect of sphereGeometry

--- src/geometry/sphere_geometry.cpp
+++ src/geometry/sphere_geometry.cpp
@@ -4,10 +4,22 @@ namespace RT_ISICG
 {
 	bool SphereGeometry::intersect( const Ray & p_ray, float & p_t1, float & p_t2 ) const
 	{
-		/// TODO !
+		Vec3f oc = p_ray.getOrigin() - _center;
+		float b	 = 2 * glm::dot( oc, p_ray.getDirection() );
+		float c	 = glm::dot( oc, oc ) - _radius * _radius;
+		float delta = b * b - 4 * c;
 
-		p_t1 = -1.f;
-		p_t2 = -1.f;
+		if ( delta >= 0 ) { 
+			float p_t1 = ( -b - glm::sqrt( delta ) ) / 2;
+			if (p_t1 > 0) {
+				return p_t1; //si intersection devant
+			}
+			else { 
+				p_t2 = ( -b + glm::sqrt( delta ) ) / 2;
+				return p_t2 > 0 ? p_t2 : -1;
+			}
+
+		}
 
 		return false;
 	}
